3a1e16a540712a10d53ec6b9d00bc3d94ab1ef0b
Ade Sudiman <sudiman.ade@gmail.com>
2014-04-24 02:42:54 +0700

    move function transfering data from REportFormUI to ReportController

--- CrimeZone/CrimeZone/src/main/java/com/ppl/crimezone/classes/ReportController.java
+++ CrimeZone/CrimeZone/src/main/java/com/ppl/crimezone/classes/ReportController.java
@@ -2,9 +2,9 @@ package com.ppl.crimezone.classes;
 
 import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.SerializedName;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -24,6 +24,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -31,6 +32,88 @@ import java.util.List;
  */
 public class ReportController {
 
+
+    public static String[] prepareNewReportData(Calendar time, Calendar submitTime,String username, String title, String description, LatLng location, boolean [] categories){
+        String data[] = new String[7];
+        data[0] = username;
+        data[1] = title;
+        String dateString = submitTime.get(Calendar.DATE)+"";
+        if(submitTime.get(Calendar.DATE)<10)dateString = "0"+ dateString;
+        String monthString = (submitTime.get(Calendar.MONTH)+1)+"";
+        if((submitTime.get(Calendar.MONTH)+1)< 10)dateString = "0"+ monthString;
+        String hourString = submitTime.get(Calendar.HOUR_OF_DAY)+"";
+        if(submitTime.get(Calendar.HOUR_OF_DAY)<10)hourString= "0"+ hourString;
+        String minuteString = submitTime.get(Calendar.MINUTE)+"";
+        if(submitTime.get(Calendar.MINUTE )< 10)minuteString = "0"+ monthString;
+
+        data[2] = dateString+ "/"+monthString+"/" + submitTime.get(Calendar.YEAR) + " " +hourString + ":"+minuteString;
+
+        hourString = time.get(Calendar.HOUR_OF_DAY)+"";
+        if(time.get(Calendar.HOUR_OF_DAY) < 10)hourString = "0" + hourString;
+        minuteString = time.get(Calendar.MINUTE)+"";
+        if(time.get(Calendar.MINUTE) < 10)minuteString = "0"+ minuteString;
+        data[3] =  dateString+"/"+monthString+"/" + time.get(Calendar.YEAR) + " " + hourString+ ":"+ minuteString;
+
+        data[4] = description;
+        data[5] = location.latitude+"";
+        data[6] = location.longitude+"";
+        for(int jj=0; jj<8;  ++jj){
+            data[jj+6] = categories[jj]+"";
+        }
+        return data;
+    }
+    public static String postData(String valueIWantToSend[]) {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://crimezone.besaba.com/webservice/submitReport.php");
+        String message = "";
+        try
+        {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("username", valueIWantToSend[0]));
+            nameValuePairs.add(new BasicNameValuePair("title", valueIWantToSend[1]));
+            nameValuePairs.add(new BasicNameValuePair("dreported", valueIWantToSend[2]));
+            nameValuePairs.add(new BasicNameValuePair("time", valueIWantToSend[3]));
+            nameValuePairs.add(new BasicNameValuePair("description", valueIWantToSend[5]));
+            nameValuePairs.add(new BasicNameValuePair("lat", valueIWantToSend[6]));
+            nameValuePairs.add(new BasicNameValuePair("long", valueIWantToSend[7]));
+            if(valueIWantToSend[8].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype1", 0+""));
+            }
+            if(valueIWantToSend[9].equals("true")) {
+                nameValuePairs.add(new BasicNameValuePair("crimetype2", 1 + ""));
+            }
+            if(valueIWantToSend[10].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype3", 2+""));
+            }
+            if(valueIWantToSend[11].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype4", 3+""));
+            }
+            if(valueIWantToSend[12].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype5", 4+""));
+            }
+            if(valueIWantToSend[13].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype6", 5+""));
+            }
+            if(valueIWantToSend[14].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype7", 6+""));
+            }
+            if(valueIWantToSend[15].equals("true")){
+                nameValuePairs.add(new BasicNameValuePair("crimetype8", 7+""));
+            }
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            message =  response.getStatusLine().getStatusCode()+"";
+        }
+        catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+        return message;
+    }
 
     public static CrimeReport fetchReportDetail(String reportId){
 
@@ -78,13 +161,7 @@ public class ReportController {
             Log.d("Error", e.toString());
             e.printStackTrace();
         }
-
         return detailReport;
-    }
-
-    private class RatingTransfer{
-        @SerializedName("rate_value")
-        double rateScore;
     }
 
 
@@ -122,8 +199,8 @@ public class ReportController {
                     Gson gson = gsonBuilder.create();
                     Log.d("gson : ", "sebelum gson from json");
 
-                    RatingTransfer temp = gson.fromJson(reader, RatingTransfer.class);
-                    newRating = new Double(temp.rateScore);
+                    CrimeReport updatedReport = gson.fromJson(reader, CrimeReport.class);
+                    newRating = new Double(updatedReport.getAvgScore());
                     if(newRating != null){
                         Log.d("new rating", newRating.toString());
 
